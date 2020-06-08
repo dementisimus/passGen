3c5bf7b8afeb0bd99218b4e4bc2498c48c79dc85
dementisimus <54750441+dementisimus@users.noreply.github.com>
2020-06-08 22:41:17 +0200

    Update passGen.py

--- passGen.py
+++ passGen.py
@@ -7,7 +7,9 @@ def copy(m):
     command = 'echo | set /p nul=' + m.strip() + '| clip'
     os.system(command);
 
-a = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%*+,-.:;?@_';
+a = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+if input("use Aa-Zz-09 only (y/n): ") == "n":
+	a += '!#$%*+,-.:;?@_';
 pwd = ''.join(secrets.choice(a) for i in range(int(input("Length: "))));
 copy(pwd);
 sys.exit();
