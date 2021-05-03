6d4a7ac7da48b6962a332781ceb262783349d507
dementisimus <54750441+dementisimus@users.noreply.github.com>
2021-05-03 14:30:59 +0200

    add cross platform support

--- passGen.py
+++ passGen.py
@@ -1,15 +1,10 @@
-import secrets;
-import string;
-import os;
+import random;
+import clipboard;
 import sys;
 
-def copy(m):
-    command = 'echo | set /p nul=' + m.strip() + '| clip'
-    os.system(command);
-
 a = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-if input("use Aa-Zz-09 only (y/n): ") == "n":
+if raw_input("use Aa-Zz-09 only (y/n): ") == "n":
 	a += '!#$%*+,-.:;?@_';
-pwd = ''.join(secrets.choice(a) for i in range(int(input("Length: "))));
-copy(pwd);
+pwd = ''.join(random.choice(a) for i in range(int(raw_input("Length: "))))
+clipboard.copy(pwd);
 sys.exit();
